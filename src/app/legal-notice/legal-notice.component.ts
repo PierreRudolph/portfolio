48dbf31db1d939886fefb2e3542a0582f27c5bc7
PierreRudolph <Plettner@hotmail.de>
2023-07-10 17:50:28 +0200

    commit

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-legal-notice',
   templateUrl: './legal-notice.component.html',
   styleUrls: ['./legal-notice.component.scss']
 })
-export class LegalNoticeComponent {
+export class LegalNoticeComponent implements OnInit {
 
+  constructor(private scroll: ViewportScroller) {
+
+  }
+
+  ngOnInit(): void {
+    this.scroll.scrollToPosition([0, 0]);
+  }
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -25,27 +25,27 @@ export class PortfolioComponent {
     technologies: 'Angular | TypeScript | HTML | CSS | Firebase',
     info: 'Task manager inspired by the Kanban System. Create an organize tasks using drag and drop functions, assign users and categories.',
     projectLink: 'https://pierre-lettner.de/Join/index.html',
-    gitHubLink: ''
+    gitHubLink: 'https://github.com/PierreRudolph/Join'
   }, {
     img: 'pokedex.png',
     title: 'Pokedex',
     technologies: 'JavaScript | HTML | CSS | Api',
     info: 'Based on the PokéAPI a simple library that provides and catalouges pokemon information.',
     projectLink: 'https://pierre-lettner.de/Pokedex/index.html',
-    gitHubLink: ''
+    gitHubLink: 'https://github.com/PierreRudolph/Pokedex'
   }, {
     img: 'pollo-loco.png',
     title: 'El Pollo Loco',
     technologies: 'JavaScript | HTML | CSS | OOP',
     info: 'Jump and Run Game',
     projectLink: 'https://pierre-lettner.de/El-Pollo-Loco/index.html',
-    gitHubLink: ''
+    gitHubLink: 'https://github.com/PierreRudolph/Ell_Polo_loco'
   }, {
     img: 'tic-tac-toe.png',
     title: 'Tic Tac Toe',
     technologies: 'JavaScript | HTML | CSS',
     info: 'Classic Tic Tac Toe game, with self Concipated and Programmed Computer Enemy',
     projectLink: 'https://pierre-lettner.de/Tic-Tac-Toe/index.html',
-    gitHubLink: ''
+    gitHubLink: 'https://github.com/PierreRudolph/TicTacToe'
   }]
 }
